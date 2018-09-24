af1646d61c899a9e5f09ebe28a77e1d703175183
Fengxu_W <wangfengxu@outlook.com>
2018-09-24 13:29:09 +0800

    健身模式显示部分
    侧边栏、canvas圆形进度条

--- pages/fit_clock/fit_clock.js
+++ pages/fit_clock/fit_clock.js
@@ -35,40 +35,26 @@ function formatSeconds(that) {
 
 Page({
   data: {
-    min: "00",
-    seconds: "00",
-    ms: "00",
-    data: "00",
-    time: "00''00'00",
     btn_counter_name: '启动',
     btn_counter_type: 'primary',
     btn_color: '',
-    record_list: [],
-    scrollTop: 0,
-    //toast默认不显示 
-    isShowToast: false
-  },
-  insert: function (event){
-    var status = this.data.btn_counter_name;
-    if (status == '停止' && event.target.id !== "btn_counter") {
-      var record_list = this.data.record_list;
-      var index = record_list.length + 1;
-      var record = this.data.time;
-      record_list.push({ 'index': index, 'record': record })
-      this.setData({
-        record_list: record_list
-      });
-      // 新增记录时，跳到顶部
-      var _top = this.data.scrollTop;//发现设置scroll-top值不能和上一次的值一样，否则无效
-      if (_top == 1) {
-        _top = 0;
-      } else {
-        _top = 1;
-      }
-      this.setData({
-        'scrollTop.scroll_top': _top
-      });
-    }
+    isShowToast: false,
+    // 滑动侧边栏
+    open: false,
+    mark: 0,
+    newmark: 0,
+    istoright: true,
+    // picker
+    picker_off: false,
+    groupArray: [2, 3, 4, 5, 6],
+    relaxArray: [10, 20, 30, 90, 180, 300],
+    timeArray: [30, 60, 90, 120, 150, 180, 210, 240, 270, 300],
+    groupSum: 2,
+    groupTime: 30,
+    relaxTime: 10,
+    // 倒计时数据
+    groupNum: 1,
+    time: 30
   },
   click_counter: function () {
     var status = this.data.btn_counter_name;
@@ -102,7 +88,131 @@ Page({
       });
     }
   },
+  // 点击组数组件确定事件 
+  bindGroupNumChange: function (e) {
+    this.setData({
+      groupSum: this.data.groupArray[e.detail.value]
+    });
+  },
+  bindRelaxChange: function (e) {
+    this.setData({
+      relaxTime: this.data.relaxArray[e.detail.value]
+    });
+  },
+  bindTimeChange: function (e) {
+    var groupTime = this.data.timeArray[e.detail.value]
+    this.setData({
+      groupTime: groupTime,
+      time: groupTime
+    });
+  },
+  tap_ch: function (e) {
+    if (this.data.open) {
+      this.setData({
+        open: false
+      });
+      var _this = this;
+      setTimeout(function () {
+        _this.drawProgressbg();
+        _this.drawCircle(0.2);
+      }, 500)
+    } else {
+      this.setData({
+        open: true
+      });
+      this.clearCircle()
+    }
+  },
+  tap_start: function (e) {
+    // touchstart事件
+    this.data.mark = this.data.newmark = e.touches[0].pageX;
+  },
+  tap_drag: function (e) {
+    // touchmove事件
+
+    /*
+     * 手指从左向右移动
+     * @newmark是指移动的最新点的x轴坐标 ， @mark是指原点x轴坐标
+     */
+    this.data.newmark = e.touches[0].pageX;
+    if (this.data.mark < this.data.newmark) {
+      this.istoright = true;
+    }
+    /*
+     * 手指从右向左移动
+     * @newmark是指移动的最新点的x轴坐标 ， @mark是指原点x轴坐标
+     */
+    if (this.data.mark > this.data.newmark) {
+      this.istoright = false;
+
+    }
+    this.data.mark = this.data.newmark;
+
+  },
+  tap_end: function (e) {
+    // touchend事件
+    this.data.mark = 0;
+    this.data.newmark = 0;
+    // this.clearCircle();
+    if (this.istoright) {
+      this.setData({
+        open: true
+      });
+      this.clearCircle()
+    } else {
+      this.setData({
+        open: false
+      });
+      var _this = this;
+      setTimeout(function () {
+        _this.drawProgressbg();
+        _this.drawCircle(0.2);
+        }, 500)
+    }
+  },
   onLoad: function () {
-  
+  },
+  drawProgressbg: function () {
+    // 使用 wx.createContext 获取绘图上下文 context
+    var ctx = wx.createCanvasContext('canvasProgressbg')
+    ctx.setLineWidth(6);// 设置圆环的宽度
+    ctx.setStrokeStyle('#d2d2d2'); // 设置圆环的颜色
+    ctx.setLineCap('round') // 设置圆环端点的形状
+    ctx.beginPath();//开始一个新的路径
+    ctx.arc(125, 125, 120, 0, 2 * Math.PI, false);
+    //设置一个原点(110,110)，半径为100的圆的路径到当前路径
+    ctx.stroke();//对当前路径进行描边
+    ctx.draw();
+  },
+  drawCircle: function (step) {
+    var context = wx.createCanvasContext('canvasProgress');
+    // 设置渐变
+    var gradient = context.createLinearGradient(200, 100, 100, 200);
+    gradient.addColorStop("0", "#2661DD");
+    gradient.addColorStop("0.5", "#40ED94");
+    gradient.addColorStop("1.0", "#fb9126");
+
+    context.setLineWidth(6);
+    context.setStrokeStyle('#fb9126'); // 设置圆环的颜色
+    // context.setStrokeStyle(gradient);
+    context.setLineCap('round')
+    context.beginPath();
+    // 参数step 为绘制的圆环周长，从0到2为一周 。 -Math.PI / 2 将起始角设在12点钟位置 ，结束角 通过改变 step 的值确定
+    context.arc(125, 125, 120, -Math.PI / 2, step * Math.PI - Math.PI / 2, false);
+    context.stroke();
+    context.draw()
+  },
+  clearCircle: function (){
+    console.log('clear')
+    var ctx = wx.createCanvasContext('canvasProgress')
+    ctx.clearRect(0, 0, 250, 250)
+    ctx.draw()
+    var ctx1 = wx.createCanvasContext('canvasProgressbg')
+    ctx1.clearRect(0, 0, 250, 250)
+    ctx1.draw()
+  },
+  onReady: function () {
+    this.drawProgressbg();
+    this.drawCircle(0.2);
   }
 })
